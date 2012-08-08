3bde809ff3ab20a2307a763d94c2002b96f8cbd5
Adam Wead <amsterdamos@gmail.com>
2012-08-08 11:47:45 -0400

    More definitions

--- lib/ead_mapper.rb
+++ lib/ead_mapper.rb
@@ -1,7 +1,7 @@
 class EadMapper < Solrizer::FieldMapper
 
   id_field 'id'
-  index_as :searchable do |t|
+  index_as :searchable, :default => true do |t|
     t.default :suffix => '_t'
     t.date    :suffix => '_dt'
     t.string  :suffix => '_t'
@@ -17,7 +17,5 @@ class EadMapper < Solrizer::FieldMapper
   index_as :facetable,             :suffix => '_facet'
   index_as :sortable,              :suffix => '_sort'
   index_as :unstemmed_searchable,  :suffix => '_unstem_search'
-  index_as :single_string,         :suffix => '_s'
-  index_as :text,:default => true, :suffix => '_t'
 
 end
--- lib/solr_ead/document.rb
+++ lib/solr_ead/document.rb
@@ -7,7 +7,7 @@ class Document
 
   # Define each term in your ead that you want put into the solr document
   set_terminology do |t|
-    t.root(:path=>"ead", :index_as => [:not_text])
+    t.root(:path=>"ead", :index_as => [:not_searchable])
 
     t.eadid
     t.corpname(:index_as=>[:facetable])
@@ -19,19 +19,46 @@ class Document
     t.subject(:index_as=>[:facetable])
 
     t.title(:path=>"titleproper", :attributes=>{ :type => :none })
+    t.title_filing(:path=>"titleproper", :attributes=>{ :type => "filing" }, :index_as=>[:not_searchable, :sortable])
 
+    # General field available within archdesc
+    t.accessrestrict(:path=>"archdesc/accessrestrict/p")
+    t.accessrestrict_heading(:path=>"archdesc/accessrestrict/head", :index_as=>[:not_searchable, :displayable])
+    t.accruals(:path=>"archdesc/accruals/p")
+    t.accruals_heading(:path=>"archdesc/accruals/head", :index_as=>[:not_searchable, :displayable])
+    t.acqinfo(:path=>"archdesc/acqinfo/p")
+    t.acqinfo_heading(:path=>"archdesc/acqinfo/head", :index_as=>[:not_searchable, :displayable])
+    t.altformavail(:path=>"archdesc/altformavail/p")
+    t.altformavail_heading(:path=>"archdesc/altformavail/head", :index_as=>[:not_searchable, :displayable])
+    t.appraisal(:path=>"archdesc/appraisal/p")
+    t.appraisal_heading(:path=>"archdesc/appraisal/head", :index_as=>[:not_searchable, :displayable])
+    t.arrangement(:path=>"archdesc/arrangement/p")
+    t.arrangement_heading(:path=>"archdesc/arrangement/head", :index_as=>[:not_searchable, :displayable])
+    t.custodhist(:path=>"archdesc/custodhist/p")
+    t.custodhist_heading(:path=>"archdesc/custodhist/head", :index_as=>[:not_searchable, :displayable])
+    t.fileplan(:path=>"archdesc/fileplan/p")
+    t.fileplan_heading(:path=>"archdesc/fileplan/head", :index_as=>[:not_searchable, :displayable])
+    t.originalsloc(:path=>"archdesc/originalsloc/p")
+    t.originalsloc_heading(:path=>"archdesc/originalsloc/head", :index_as=>[:not_searchable, :displayable])
+    t.phystech(:path=>"archdesc/phystech/p")
+    t.phystech_heading(:path=>"archdesc/phystech/head", :index_as=>[:not_searchable, :displayable])
+    t.processinfo(:path=>"archdesc/processinfo/p")
+    t.processinfo_heading(:path=>"archdesc/processinfo/head", :index_as=>[:not_searchable, :displayable])
+    t.relatedmaterial(:path=>"archdesc/relatedmaterial/p")
+    t.relatedmaterial_heading(:path=>"archdesc/relatedmaterial/head", :index_as=>[:not_searchable, :displayable])
     t.separatedmaterial(:path=>"archdesc/separatedmaterial/p")
-    t.separatedmaterial_heading(:path=>"archdesc/separatedmaterial/head", :index_as=>[:not_text, :displayable])
+    t.separatedmaterial_heading(:path=>"archdesc/separatedmaterial/head", :index_as=>[:not_searchable, :displayable])
     t.scopecontent(:path=>"archdesc/scopecontent/p")
-    t.scopecontent_heading(:path=>"archdesc/scopecontent/head", :index_as=>[:not_text, :displayable])
-
+    t.scopecontent_heading(:path=>"archdesc/scopecontent/head", :index_as=>[:not_searchable, :displayable])
+    t.userestrict(:path=>"archdesc/userestrict/p")
+    t.userestrict_heading(:path=>"archdesc/userestrict/head", :index_as=>[:not_searchable, :displayable])
 
   end
 
   def to_solr(solr_doc = Hash.new)
     super(solr_doc)
-    solr_doc.merge!({"id" => self.eadid.first})
-    solr_doc.merge!({"eadid_s" => self.eadid.first})
+    solr_doc.merge!({"id"          => self.eadid.first})
+    solr_doc.merge!({"eadid_s"     => self.eadid.first})
     solr_doc.merge!({"xml_display" => self.to_xml})
     return solr_doc
   end
