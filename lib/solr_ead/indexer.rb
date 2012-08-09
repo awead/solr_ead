d7ed0c95c37dc54c2a9c45ffe4777ec8ea722d44
Adam Wead <amsterdamos@gmail.com>
2012-08-09 12:44:50 -0400

    Added a simple option

--- lib/solr_ead/indexer.rb
+++ lib/solr_ead/indexer.rb
@@ -10,7 +10,7 @@ class Indexer
   include RSolr
   include SolrEad::Behaviors
 
-  attr_accessor :solr
+  attr_accessor :solr, :opts
 
   # Creates a new instance of SolrEad::Indexer and connects to your solr server
   # using the url supplied in your config/solr.yml file.
@@ -20,35 +20,36 @@ class Indexer
     else
       self.solr = RSolr.connect :url => YAML.load_file("config/solr.yml")['development']['url']
     end
+    self.opts = opts
   end
 
   # Indexes your ead and additional component documents with the supplied file, then
   # commits the results to your solr server.
-  def create(file,opts={})
+  def create(file)
     doc = EadDocument.from_xml(File.new(file))
     solr_doc = doc.to_solr
-    self.solr.add solr_doc
-    add_components file
-    self.solr.commit
+    solr.add solr_doc
+    add_components(file) unless opts[:simple]
+    solr.commit
   end
 
   # Updates your ead from a given file by first deleting the existing ead document and
   # any component documents, then creating a new index from the supplied file.
   # This method will also commit the results to your solr server when complete.
-  def update(file,opts={})
+  def update(file)
     doc = EadDocument.from_xml(File.new(file))
     solr_doc = doc.to_solr
-    self.solr.delete_by_query( 'eadid_s:"' + solr_doc["id"] + '"' )
-    self.solr.add solr_doc
-    add_components file
-    self.solr.commit
+    solr.delete_by_query( 'eadid_s:"' + solr_doc["id"] + '"' )
+    solr.add solr_doc
+    add_components(file) unless opts[:simple]
+    solr.commit
   end
 
   # Deletes the ead document and any component documents from your solr index and
   # commits the results.
   def delete(id)
-    self.solr.delete_by_query( 'eadid_s:"' + id + '"')
-    self.solr.commit
+    solr.delete_by_query( 'eadid_s:"' + id + '"')
+    solr.commit
   end
 
   protected
@@ -65,7 +66,7 @@ class Indexer
     components(file).each do |node|
       solr_doc = EadComponent.from_xml(prep(node)).to_solr(additional_component_fields(node))
       solr_doc.merge!({"sort_i" => counter.to_s})
-      self.solr.add solr_doc
+      solr.add solr_doc
       counter = counter + 1
     end
   end
--- spec/indexer_spec.rb
+++ spec/indexer_spec.rb
@@ -6,26 +6,55 @@ describe SolrEad::Indexer do
     @file = fixture "ARC-0005.xml"
   end
 
-  before :each do
-    @indexer = SolrEad::Indexer.new
-  end
+  describe "indexing an ead document into multiple solr documents" do
 
-  describe "#create" do
+    before :each do
+      @indexer = SolrEad::Indexer.new
+    end
+
     it "should index a new ead from a file" do
       @indexer.create(@file)
+      q = @indexer.solr.get 'select', :params => {:q=>'eadid_s:"ARC-0005"', :qt=>'document'}
+      q["response"]["numFound"].should == 136
     end
-  end
 
-  describe "#update" do
+
     it "should update an ead from a file" do
       @indexer.update(@file)
+      q = @indexer.solr.get 'select', :params => {:q=>'eadid_s:"ARC-0005"', :qt=>'document'}
+      q["response"]["numFound"].should == 136
     end
-  end
 
-  describe "#delete" do
     it "should delete and ead give an id" do
       @indexer.delete("ARC-0005")
+      q = @indexer.solr.get 'select', :params => {:q=>'eadid_s:"ARC-0005"', :qt=>'document'}
+      q["response"]["numFound"].should == 0
     end
+
+  end
+
+  describe "indexing ead document into one solr document" do
+
+    before :each do
+      @simple_indexer = SolrEad::Indexer.new(:simple => true)
+    end
+
+    it "should be set to use the simple option" do
+      @simple_indexer.opts[:simple].should be_true
+    end
+
+    it "should index a new ead from a file as a single solr document" do
+      @simple_indexer.create(@file)
+      q = @simple_indexer.solr.get 'select', :params => {:q=>'eadid_s:"ARC-0005"', :qt=>'document'}
+      q["response"]["numFound"].should == 1
+    end
+
+    it "should delete a single ead" do
+      @simple_indexer.delete("ARC-0005")
+      q = @simple_indexer.solr.get 'select', :params => {:q=>'eadid_s:"ARC-0005"', :qt=>'document'}
+      q["response"]["numFound"].should == 0
+    end
+
   end
 
 end
